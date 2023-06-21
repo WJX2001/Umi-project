ad8d2ce425359a8b363a517393835f1eec7d1b01
WJX2001 <1758807220@qq.com>
2023-06-21 14:12:13 +0800

    feat: 通过运行配置控制用户登录访问权限

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 // 在这个文件中做项目的运行时配置
 
 import { message } from 'antd';
+import './utils/init-leancloud-sdk'; //初始化leanCloud的sdk
+import {
+  BasicLayoutProps,
+  Settings as LayoutSettings,
+} from '@ant-design/pro-layout';
+
+import { history } from 'umi';
 
 // 异步请求相关运行时配置
 export const request = {
@@ -35,4 +42,29 @@ export const request = {
       return { data }; // 此处return 的内容是后端下发的数据包
     },
   ],
+};
+
+// 初始化全局数据的运行时配置
+export async function getInitialState() {
+  let userState = {
+    isLogin: false,
+    userInfo: null,
+  };
+  console.log('getInitialState运行时配置', userState);
+  return userState;
+}
+
+// layout的运行时配置，自定义控制Layout的渲染逻辑
+export const layout = ({ initialState }: any) => {
+  return {
+    onPageChange: () => {
+      // 此处可以根据用户的登录状态引导用户进行指定的访问
+      console.log('onPageChange', initialState);
+      let { isLogin } = initialState;
+      if (!isLogin) {
+        // 此时没有登录
+        history.push('/login');
+      }
+    },
+  };
 };
